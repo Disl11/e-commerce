04959787775841a2830b6c09a8b716dee6ff4f3c
Valentin <218587221+val277@users.noreply.github.com>
2025-08-25 16:46:45 +0200

    fix id

--- assets/js/shared/utils.js
+++ assets/js/shared/utils.js
@@ -1,3 +1,5 @@
 // helpers communs (DOM, formatage, URL id)
 // sélection DOM, format prix, lecture id d'URL
 export const clearInputValue = (input) => (input.value = "");
+
+export const getIdFromUrl = () => window.location.href.split("=")[1];
